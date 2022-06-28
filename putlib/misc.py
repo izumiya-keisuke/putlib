5cbd5e21cba7aba2492fdee60d256358091b81ab
Keisuke Izumiya <k.izumiya.13@gmail.com>
2022-06-28 17:03:58 +0900

    add chdir, tmpdir

--- putlib/misc.py
+++ putlib/misc.py
@@ -14,6 +14,40 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+
+class Chdir:
+    def __init__(self, path: Path) -> None:
+        self._original_path: Path = Path.cwd()
+        self._new_path: Path = path
+
+    def __enter__(self) -> None:
+        os.chdir(self._new_path)
+
+    def __exit__(self, _0: any, _1: any, _2: any) -> None:
+        os.chdir(self._original_path)
+
+
+class TmpDir:
+    def __init__(self) -> None:
+        self._tmp_dir: TemporaryDirectory = TemporaryDirectory()
+        self.path: Path = Path(self._tmp_dir.name)
+
+    def __del__(self) -> None:
+        self._cleanup()
+
+    def __enter__(self) -> Path:
+        return self.path
+
+    def __exit__(self, _0: any, _1: any, _2: any) -> None:
+        self._cleanup()
+
+    def _cleanup(self) -> None:
+        self._tmp_dir.cleanup()
+
 
 def id_fn(x: any) -> any:
     return x
